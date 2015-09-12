783bf6cf01328ff446cf5fd328fbcd73d129fa4a
Ranjana Choudhary <rockingrails@gmail.com>
2015-09-12 16:42:00 +0530

    Convert review rating to stars

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,14 @@ class MoviesController < ApplicationController
 
   
   def show
+    @reviews= Review.where(movie_id: @movie.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_review=0
+    else
+      @avg_review =@reviews.average(:rating).round(2)
   end
-
-  
-  def new
+end
+def new
     @movie = current_user.movies.build
   end
 
