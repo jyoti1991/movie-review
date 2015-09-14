abb350207227747f38594395603f22b69dd7c430
Ranjana Choudhary <rockingrails@gmail.com>
2015-09-14 11:42:34 +0530

    validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
 		belongs_to :user
 	has_many :reviews
+	validates :title,:description,:director,:movie_length,:rating,:uniqueness =>true, :presence => true,length: { minimum: 3 }
 	 has_attached_file :image, styles: { medium: "400x600>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
